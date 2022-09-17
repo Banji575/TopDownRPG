54e22fd4bce1ad7f96e77e671cde93784f97c75e
Churkon Sergey <hotmerch@hotmail.com>
2022-09-18 01:22:48 +0300

    add collide and hit action between player and enemys

--- src/scripts/components/Enemy/Enemy.ts
+++ src/scripts/components/Enemy/Enemy.ts
@@ -1,8 +1,63 @@
 import Phaser from "phaser";
 
-export class Enemy extends Phaser.GameObjects.Sprite {
+enum DIRECTION {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+}
+
+export class Enemy extends Phaser.Physics.Arcade.Sprite {
+    direction: DIRECTION = DIRECTION.RIGHT
+    moveEvent: Phaser.Time.TimerEvent
+    speed: number = 65
     constructor(public scene: Phaser.Scene, x: number, y: number, texture: string, frame: string) {
         super(scene, x, y, texture, frame)
+        this.init()
+    }
+
+    init() {
+        this.anims.play('enemy-run')
+        this.scene.physics.world.on(Phaser.Physics.Arcade.Events.TILE_COLLIDE, this.#onTileCollide, this)
+        this.initSetDirTImer()
+    }
+
+    destroy(fromScene?: boolean | undefined): void {
+        this.moveEvent.destroy()
+        super.destroy(fromScene)
+    }
+
+    initSetDirTImer() {
+        this.moveEvent = this.scene.time.addEvent({
+            delay: 2000,
+            callback: () => {
+                const newDir = Phaser.Math.Between(0, 3)
+                this.direction = newDir
+            },
+            loop: true
+        })
+    }
+
+    #onTileCollide(go: Phaser.GameObjects.GameObject, tile: Phaser.Tilemaps.Tile) {
+        const newDir = Phaser.Math.Between(0, 3)
+        this.direction = newDir
+    }
+
+    preUpdate(time: number, delta: number): void {
+        super.preUpdate(time, delta)
+        switch (this.direction) {
+            case DIRECTION.RIGHT:
+                this.setVelocity(this.speed, 0)
+                break
+            case DIRECTION.LEFT:
+                this.setVelocity(-this.speed, 0)
+                break
+            case DIRECTION.UP:
+                this.setVelocity(0, -this.speed)
+                break
+            case DIRECTION.DOWN:
+                this.setVelocity(0, this.speed)
+        }
     }
 
 }
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,13 +1,20 @@
 import { AnimationController, typeAnimation } from "../components/Animation/AnimationController"
+import { Enemy } from "../components/Enemy/Enemy"
+
 
 export default class MainScene extends Phaser.Scene {
 
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys
   private character: Phaser.Physics.Arcade.Sprite
   private enemy: Phaser.GameObjects.Sprite
+  private enemys: Phaser.Physics.Arcade.Group
+  private enemySpeed: number
+  private hitCount: number
 
   constructor() {
     super({ key: 'MainScene' })
+    this.enemySpeed = 3
+
   }
 
   preload() {
@@ -36,8 +43,6 @@ export default class MainScene extends Phaser.Scene {
 
     this.character = this.physics.add.sprite(128, 128, 'character', 'run-down-1.png')
     this.character.body.setSize(this.character.width * .8, this.character.height * .8)
-
-    this.physics.add.collider(barrierLayer, this.character)
 
 
     this.cameras.main.startFollow(this.character, false, .6, .6)
@@ -86,13 +91,38 @@ export default class MainScene extends Phaser.Scene {
     this.character.anims.play('character-run-side')
 
 
+    this.enemys = this.physics.add.group({
+      classType: Enemy,
+      createCallback: go => {
+        const enemy = go as Enemy
+        enemy.body.onCollide = true
+      }
+    })
 
-    this.enemy = this.add.sprite(100, 100, 'enemy', 'knight_m_hit_anim_f0.png')
-    this.enemy.anims.play('enemy-run')
+    this.enemys.get(100, 100, 'enemy')
+    this.enemys.get(150, 150, 'enemy')
+
+    this.physics.add.collider(barrierLayer, this.character)
+    this.physics.add.collider(barrierLayer, this.enemys)
+
+    this.physics.add.collider(this.character, this.enemys, this.hitDamage, undefined, this)
+
 
   }
 
-  update(t: number, dt: number) {
+  hitDamage(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
+    const enemy = obj2 as Enemy
+
+    const dx = this.character.x - enemy.x
+    const dy = this.character.y - enemy.y
+    const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(150)
+    this.character.setVelocity(dir.x, dir.y)
+    this.hitCount = 1
+  }
+
+  inputController() {
+
+
     if (this.cursors.left.isDown) {
       this.character.setVelocity(-100, 0)
       this.character.anims.play('character-run-side', true)
@@ -116,4 +146,20 @@ export default class MainScene extends Phaser.Scene {
       this.character.anims.play(parce.join('-'), true)
     }
   }
+
+
+
+  update(t: number, dt: number) {
+    if (this.hitCount > 0) {
+      this.hitCount++
+
+      if (this.hitCount >= 10) {
+        this.hitCount = 0
+      }
+      return
+    }
+    this.inputController()
+  }
+
+
 }
